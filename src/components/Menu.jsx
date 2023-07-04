831a4d91def4b0881f132c830f2efa2e629e6ed2
Juan Carlos Barrios <tornillosbabe@hotmail.com>
2023-07-04 16:26:42 -0600

    feat:English - Spanish translation of the sections, Resume, Menu and Menu Icons.

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,17 +1,56 @@
-import '../styles/components/menu.scss'
-import { faBriefcase, faEnvelope, faGripVertical, faHouse, faShapes, faUser } from '@fortawesome/free-solid-svg-icons'
-import MenuItem from '../common/MenuItem'
+import "../styles/components/menu.scss";
+import {
+  faBriefcase,
+  faEnvelope,
+  faGripVertical,
+  faHouse,
+  faShapes,
+  faUser,
+} from "@fortawesome/free-solid-svg-icons";
+import MenuItem from "../common/MenuItem";
+import { useTranslation } from "react-i18next";
 const Menu = () => {
+  const { t } = useTranslation();
   return (
-    <div className='menu'>
-      <MenuItem icon={faHouse} id='introduce' title='Home' iconWidth={17} />
-      <MenuItem icon={faUser} id='about' title='About' iconWidth={14} />
-      <MenuItem icon={faBriefcase} id='resume' title='Resume' iconWidth={17} />
-      <MenuItem icon={faShapes} id='myskills' title='MySkills' iconWidth={17} />
-      <MenuItem icon={faGripVertical} id='portfolio' title='Portfolio' iconWidth={17} />
-      <MenuItem icon={faEnvelope} id='contact' title='Contact' iconWidth={17} />
+    <div className="menu">
+      <MenuItem
+        icon={faHouse}
+        id="introduce"
+        title={t("menuTitleH")}
+        iconWidth={17}
+      />
+      <MenuItem
+        icon={faUser}
+        id="about"
+        title={t("menuTitleA")}
+        iconWidth={14}
+      />
+      <MenuItem
+        icon={faBriefcase}
+        id="resume"
+        title={t("menuTitleR")}
+        iconWidth={17}
+      />
+      <MenuItem
+        icon={faShapes}
+        id="myskills"
+        title={t("menuTitleM")}
+        iconWidth={17}
+      />
+      <MenuItem
+        icon={faGripVertical}
+        id="portfolio"
+        title={t("menuTitleP")}
+        iconWidth={17}
+      />
+      <MenuItem
+        icon={faEnvelope}
+        id="contact"
+        title={t("menuTitleC")}
+        iconWidth={17}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Menu
+export default Menu;
--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -2,77 +2,108 @@ import Breadcrumb from "../common/Breadcrumb";
 import ResumeItem from "../common/ResumeItem";
 
 import { faBriefcase } from "@fortawesome/free-solid-svg-icons";
+import { useTranslation } from "react-i18next";
 
-import '../styles/components/Resume.scss'
+import "../styles/components/Resume.scss";
 
 const Resume = () => {
+  const { t } = useTranslation();
   return (
     <div className="container Resume-section">
-      <Breadcrumb title="Resume" icon={faBriefcase} widthIcon={12} id='resume'/>
+      <Breadcrumb
+        title={t("resumeTitle")}
+        icon={faBriefcase}
+        widthIcon={12}
+        id="resume"
+      />
       <h1>
-        Education & <span>Experience</span>
+        {t("resumeH1")}
+        <span>{t("resumeh1")}</span>
       </h1>
       <div className="Resume-timeline">
-        <ResumeItem date=' June 2022 - Present' experiences={[
-          {
-            title: 'Front-End Developer', 
-            description: 'Freelance'
-          }
-        ]} />
-        <ResumeItem date='2019 - February 2023' experiences={[
-          {
-            title: 'Judicial Power of the Federation.', 
-            description: 'Specialized Analyst.'
-          }, 
-          {
-            title: 'Council of the Federal Judicature.',
-            description: 'Protection of Magistrates of the Federal Government.',
-          }
-        ]} />
-        <ResumeItem date='2017 - 2019' experiences={[
-          {
-            title: 'Judicial Power of the Federation.', 
-            description: 'Middle Management Assistant.'
-          }, 
-          {
-            title: 'Electoral Tribunal of the Federal Judiciary.',
-            description: 'Protection of Magistrates of the Federal Government.'
-          }
-        ]} />
-        <ResumeItem date='2014 - 2017' experiences={[
-          {
-            title: 'Judicial Power of the Federation.', 
-            description: 'Security Officer.'
-          }, 
-          {
-            title: 'Electoral Tribunal of the Federal Judiciary.',
-            description: 'Protection of Real Estate, Assets and Officials of the Federal Government.',
-          }
-        ]} />
-        <ResumeItem date='2007 - 2014' experiences={[
-          {
-            title: 'Federal Public Security Secretary.', 
-            description: 'Personal Security to Government Officials.'
-          }, 
-          {
-            title: 'Federal Protection Service.',
-            description: 'Intramural Guard in Federal Buildings and Strategic Facilities.'
-          }
-        ]} />
-        <ResumeItem date='1999 - 2007' experiences={[
-          {
-            title: 'Secretariat of Finance and Public Credit.', 
-            description: 'Federal Tax Police.'
-          }, 
-          {
-            title: 'General Administration of Customs.',
-            description: 'Review of Foreign trade merchandise for the payment of taxes.',
-          },
-          {
-            title: 'Tax Administration Service (SAT).',
-            description: 'Review of petitions in different Customs of the country, checking the import or export of merchandise and vehicles, complying with the payment of taxes.'
-          }
-        ]} />
+        <ResumeItem
+          date={t("resumeItemDate")}
+          experiences={[
+            {
+              title: t("resumeItemTitle"),
+              description: t("resumeItemDescription"),
+            },
+          ]}
+        />
+        <ResumeItem
+          date={t("resumeItemDate1")}
+          experiences={[
+            {
+              title: t("resumeItemTitle1"),
+              description: t("resumeItemDescription1"),
+            },
+            {
+              title: t("resumeItemTitle2"),
+              description: t("resumeItemDescription2"),
+            },
+          ]}
+        />
+        <ResumeItem
+          date="2017 - 2019"
+          experiences={[
+            {
+              title: "Judicial Power of the Federation.",
+              description: "Middle Management Assistant.",
+            },
+            {
+              title: "Electoral Tribunal of the Federal Judiciary.",
+              description:
+                "Protection of Magistrates of the Federal Government.",
+            },
+          ]}
+        />
+        <ResumeItem
+          date="2014 - 2017"
+          experiences={[
+            {
+              title: "Judicial Power of the Federation.",
+              description: "Security Officer.",
+            },
+            {
+              title: "Electoral Tribunal of the Federal Judiciary.",
+              description:
+                "Protection of Real Estate, Assets and Officials of the Federal Government.",
+            },
+          ]}
+        />
+        <ResumeItem
+          date="2007 - 2014"
+          experiences={[
+            {
+              title: "Federal Public Security Secretary.",
+              description: "Personal Security to Government Officials.",
+            },
+            {
+              title: "Federal Protection Service.",
+              description:
+                "Intramural Guard in Federal Buildings and Strategic Facilities.",
+            },
+          ]}
+        />
+        <ResumeItem
+          date="1999 - 2007"
+          experiences={[
+            {
+              title: "Secretariat of Finance and Public Credit.",
+              description: "Federal Tax Police.",
+            },
+            {
+              title: "General Administration of Customs.",
+              description:
+                "Review of Foreign trade merchandise for the payment of taxes.",
+            },
+            {
+              title: "Tax Administration Service (SAT).",
+              description:
+                "Review of petitions in different Customs of the country, checking the import or export of merchandise and vehicles, complying with the payment of taxes.",
+            },
+          ]}
+        />
       </div>
     </div>
   );
